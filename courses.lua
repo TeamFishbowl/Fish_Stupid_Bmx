66a541a1eed886b342f71725250d33c05c843632
TeamFishbowl <57051466+TeamFishbowl@users.noreply.github.com>
2021-04-29 23:16:16 +0800

    Add files via upload

---
+++ courses.lua
@@ -0,0 +1,47 @@
+races = {
+
+            {
+        title = "BMX Medium 1",
+        isEnabled = true,
+        showWaypoints = false,
+        checkpointRadius = 2.0,
+        checkpointTransparency = 5.0,
+        mapBlipId = 226,
+        mapBlipColor = 2,
+        start = { x = -793.51, y = 360.97, z = 87.26, heading = 280.44, type = 5 },
+        checkpoints = {                                                                                         
+
+            { x = -725.81, y = 356.66, z = 95.71, heading = 76.39, type = 9 }       -- Race checkpoints, type 9 is a finish line (/saveRaceEnd)
+        }
+    },
+
+        {
+        title = "BMX Hard 1",
+        isEnabled = true,
+        showWaypoints = false,
+        checkpointRadius = 2.0,
+        checkpointTransparency = 5.0,
+        mapBlipId = 226,
+        mapBlipColor = 2,
+        start = { x = 519.21, y = -1468.14, z = 29.17, heading = 4.54, type = 5 },
+        checkpoints = {                                                                                         
+
+            { x = 514.67, y = -1408.00, z = 37.19, heading = 135.0, type = 9 }       -- Race checkpoints, type 9 is a finish line (/saveRaceEnd)
+        }
+    },
+
+    {
+        title = "BMX Easy 1",
+        isEnabled = true,
+        showWaypoints = false,
+        checkpointRadius = 2.0,
+        checkpointTransparency = 5.0,
+        mapBlipId = 226,
+        mapBlipColor = 2,
+        start = { x = -13.36, y = 209.32, z = 106.27, heading = 56.8, type = 5 },
+        checkpoints = {                                                                                         
+            -- { x = -59.64, y = 221.75, z = 106.55, heading = 88.47, type = 5 },         -- Race checkpoints, type 5 is a normal checkpoint (/saveRaceCp)
+            { x = -74.44, y = 222.82, z = 111.66, heading = 135.0, type = 9 } 
+        }
+    }
+}
